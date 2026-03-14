2647caca0c8c319d58b2635209b756364d12faa0
bsekinaev <bsekinaev@ya.ru>
2026-03-14 18:15:26 +0300

    CI_CD

---
+++ logistic/views.py
@@ -0,0 +1,27 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'description']
+    pagination_class = StandardResultsSetPagination
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['products']
+    search_fields = ['positions__product__title', 'positions__product__description']
+    pagination_class = StandardResultsSetPagination
