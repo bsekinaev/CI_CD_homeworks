8f5cbeec4eb4c2cc5df1c86107f3f68791d55eae
bsekinaev <bsekinaev@ya.ru>
2026-03-15 15:18:50 +0300

    deploy.yml

--- logistic/tests.py
+++ logistic/tests.py
@@ -1,16 +1,12 @@
-from unittest import TestCase
-
-
-class APIClient:
-    pass
-
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 
 class MyTest(TestCase):
-    def test(self):
-        self.assertTrue(True)
+    def setUp(self):
+        self.client = APIClient()
 
     def test_sample_view(self):
-        url = "/api/v1/test/"
-        client = APIClient()
-        response=client.get(url)
-        self.assertEqual(response.status_code,200)
+        url = '/api/v1/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
